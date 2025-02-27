7d028d7645b06ec4fd30f6aefcc7341d2f9f5850
Daniel Chudnov <dlchudnov@mitre.org>
2025-02-27 12:32:34 -0500

    added vista for symbols view

---
+++ lua/custom/plugins/vista.lua
@@ -0,0 +1,10 @@
+return {
+  'liuchengxu/vista.vim',
+  -- init = function()
+  --   vim.g.vista_cursor_delay = 50
+  --   vim.g.vista_renderer_enable_icon = 1
+  -- end,
+  keys = {
+    { '<leader>cs', '<cmd>Vista!!<cr>', desc = 'Symbols toggle' },
+  },
+}
